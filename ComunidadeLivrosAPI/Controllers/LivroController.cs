d4e6be44448121059543c5f6ee1753b2ece3c493
Izabella Aro <izabella.aro20@gmail.com>
2023-11-13 18:08:41 -0300

    adiciona consulta e paginacao

--- ComunidadeLivrosAPI/Controllers/LivroController.cs
+++ ComunidadeLivrosAPI/Controllers/LivroController.cs
@@ -22,7 +22,36 @@ public class LivroController : Controller
         var livro = new Livro(livroDto.Titulo, livroDto.Genero, livroDto.Autor, livroDto.QntPag);
         _context.Livros.Add(livro);
         _context.SaveChanges();
-        return Ok();
+        return NoContent();
     }
 
+    [HttpGet]
+    public IEnumerable<ReadLivroDto> ConsultaLivros([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        var consultaLivros = _context.Livros.Skip(skip).Take(take);
+
+        return consultaLivros.Select(livro =>
+            new ReadLivroDto
+            {
+                Autor = livro.Autor,
+                Genero = livro.Genero,
+                qntPag = livro.QntPag,
+                Titulo = livro.Titulo
+            }).ToList();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult ConsultaLivroPorId(int id)
+    {
+        var livro = _context.Livros.FirstOrDefault(livro => livro.Id == id);
+        if (livro == null) return NotFound();
+        var livroDto = new ReadLivroDto
+        {
+            Autor = livro.Autor,
+            Genero = livro.Genero,
+            qntPag = livro.QntPag,
+            Titulo = livro.Titulo
+        };
+        return Ok(livroDto);
+    }
 }
---
+++ ComunidadeLivrosAPI/Dtos/ReadLivroDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ComunidadeLivrosAPI.Dtos;
+
+public class ReadLivroDto
+{
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public string Autor { get; set; }
+    public int qntPag { get; set; }
+}
